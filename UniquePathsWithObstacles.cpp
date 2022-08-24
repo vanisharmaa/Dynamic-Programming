52e1962e11562ee2c7dfc9fa3ac59f555d7a550e
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-08-24 19:35:57 +0530

    Tabulation + Memoization

--- UniquePathsWithObstacles.cpp
+++ UniquePathsWithObstacles.cpp
@@ -1,4 +1,27 @@
 int mod = int(1e9 + 7);
+//TABULATION
+int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
+    // Write your code here
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[0][0] = 1;
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (i == 0 && j == 0)
+                continue;
+            int up = 0, left = 0;
+            if (i > 0 && mat[i-1][j] != -1)
+                up = dp[i-1][j];
+            if (j > 0 && mat[i][j-1] != -1)
+                left = dp[i][j-1];
+            dp[i][j] = (up + left)%mod;
+        }    
+    }
+    return dp[n-1][m-1];
+}
+
+
+
+//MEMOIZATION
 int helper(int n, int m, vector<vector<int>> &mat, vector<vector<int>> &dp){
     if(n==0 && m == 0)
         return dp[0][0] = 1;
