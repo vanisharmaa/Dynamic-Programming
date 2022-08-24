6330eab9a96e356a31574122d48573227ed9f9f4
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-08-24 15:41:58 +0530

    Tablulation + Memoization Solution

--- ninjasTraining.cpp
+++ ninjasTraining.cpp
@@ -1,3 +1,35 @@
+//TABULATION SOLUTION
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+    for(int i = 0; i < 3; i++){
+        dp[0][0] = max(points[0][1], points[0][2]);
+        dp[0][1] = max(points[0][0], points[0][2]);
+        dp[0][2] = max(points[0][1], points[0][0]);
+        dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+    }
+    for(int day = 1; day < n; day++){
+        for (int last = 0; last < 4; last++){
+            for(int task = 0; task < 3; task++){
+                if (task != last){
+                    int activity = points[day][task] + dp[day-1][task];
+                    if (activity > dp[day][last])
+                        dp[day][last] = x;
+                }
+            }
+        }       
+    }
+    return dp[n-1][3];
+}
+
+
+//MEMOIZATION SOLUTION
+
 #include<bits/stdc++.h>
 using namespace std;
 
