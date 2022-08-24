1906fdef4f4751a86510a37ef6186e7109ad65cd
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-08-24 16:05:54 +0530

    Memoization + Tabulation

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -1,4 +1,27 @@
 #include <bits/stdc++.h>
+//TABULATION SOLUTION
+int uniquePaths(int m, int n) {
+	// Write your code here.
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    dp[0][0] = 1;
+    for(int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            if (i==0 && j == 0)
+                continue;
+            int up = 0, left = 0;
+            if (i > 0)
+                up = dp[i-1][j];
+            if (j > 0)
+                left = dp[i][j-1];
+            dp[i][j] = up + left;
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+
+
+//MEMOIZATION SOLUTION
 int helper(int m, int n, vector<vector<int>>& dp){
     if (m < 0 || n  < 0)
         return 0;
