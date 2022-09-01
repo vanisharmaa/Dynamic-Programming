43d3ec0101899e428d1785b19e1fd31c6abf8363
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-09-01 11:31:55 +0530

    Memoization + Tabulation

--- MaxPathSumInAMatrix.cpp
+++ MaxPathSumInAMatrix.cpp
@@ -9,6 +9,37 @@ Down right diagonal: (row+1, col+1)
 
 */
 
+//TABULATION
+
+int getMaxPathSum(vector<vector<int>> &matrix)
+{
+    //  Write your code here.
+    int m = matrix.size();
+    int n = matrix[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    for(int i = 0; i < n; i++){
+        dp[0][i] = matrix[0][i];
+    }
+    for(int i = 1; i < m; i++){
+        for(int j = 0; j < n; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int left = -1e9;
+                if (j > 0)
+                    left = matrix[i][j] + dp[i-1][j-1];
+                int right = -1e9;
+                if (j < n-1)
+                    right = matrix[i][j] + dp[i-1][j+1];
+                dp[i][j] = max(up, max(left, right));
+        }
+    }
+    int maxi = -1e9;
+    for(int i = 0; i < n; i++){
+        maxi = max(maxi, dp[m-1][i]);
+    }
+    return maxi;
+}
+
+
 
 //MEMOIZATION
 
