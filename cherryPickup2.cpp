f1ccdca8176eca82fc5fb6a190dd814021e9346f
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-09-13 01:20:09 +0530

    Memoization + Tabulation + Space Optimisation

--- cherryPickup2.cpp
+++ cherryPickup2.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    //SPACE OPTIMISATION
+    int cherryPickup(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<int>> prev(cols, vector<int>(cols, 0));
+        vector<vector<int>> curr(cols, vector<int>(cols, 0));
+        for(int j1 = 0 ; j1 < cols; j1++){
+            for(int j2 = 0; j2 < cols; j2++){
+                if (j1 == j2)
+                    prev[j1][j2] = grid[rows-1][j1];
+                else
+                    prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
+            }
+        }
+        for(int i = rows-2; i >= 0; i--){
+            for(int j1 = 0; j1 < cols; j1++){
+                for(int j2 = 0; j2 < cols; j2++){
+                    int maxi = -1e8;
+                    for(int dj1 = -1; dj1 <= 1; dj1++){
+                        for(int dj2 = -1; dj2 <= 1; dj2++){
+                            int ans = -1e8;
+                            if(j1+dj1 < 0 || j1+dj1 >= cols ||j2+dj2 < 0 || j2+dj2 >= cols)
+                                ;
+                            else{
+                            if (j1 == j2)
+                                ans = grid[i][j1] + prev[j1 + dj1][j2 + dj2];
+                            else
+                                ans = grid[i][j1] + grid[i][j2] + prev[j1 + dj1][j2 + dj2];
+                            maxi = max(maxi, ans);
+                            }
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            prev = curr;
+        }
+        return prev[0][cols-1];
+    }
+    
+    
     //TABULATION
     int cherryPickup(vector<vector<int>>& grid) {
         int rows = grid.size();
