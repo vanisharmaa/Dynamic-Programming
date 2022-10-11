4877158474a1468c49b89adba8a486e77f144961
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-10-12 01:10:46 +0530

    Update unboundedKnapsack.cpp

--- unboundedKnapsack.cpp
+++ unboundedKnapsack.cpp
@@ -1,3 +1,28 @@
+//TABULATION
+#include <bits/stdc++.h> 
+int unboundedKnapsack(int n, int wt, vector<int> &profit, vector<int> &weight)
+{
+    // Write Your Code Here.
+    vector<vector<int>> dp(n, vector<int>(wt+1, 0));
+    for(int W = 0; W <= wt; W++){
+        if (W == 0){
+            dp[0][W] = 0;
+        }
+        dp[0][W] = profit[0]*(W/weight[0]);
+    }
+    for(int i = 1; i < n; i++){
+        for (int W = 0; W <= wt; W++){
+            int notPick = dp[i-1][W];
+            int pick = 0;
+            if (W - weight[i] >= 0)
+                pick = profit[i] + dp[i][W - weight[i]];
+            dp[i][W] = max(pick, notPick);    
+        }
+    }
+    return dp[n-1][wt];
+}
+
+
 //MEMOIZATION
 #include <bits/stdc++.h> 
 int f(int i, int W, vector<int>& profit, vector<int>& weight, vector<vector<int>>& dp){
