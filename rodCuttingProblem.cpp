1a60a26ddb74bfdfaf9cad79af90126b3a7ab8a0
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-10-12 02:00:59 +0530

    Update rodCuttingProblem.cpp

--- rodCuttingProblem.cpp
+++ rodCuttingProblem.cpp
@@ -1,3 +1,28 @@
+//TABULATION
+int cutRod(vector<int> &price, int n)
+{
+	// Write your code here.
+    vector<vector<int>> dp(n, vector<int>(n+1, 0));
+    for(int N = 1; N <= n; N++){
+        if (n == 0)
+            dp[0][N] = 0;
+        else
+            dp[0][N] = price[0]*N;
+    }
+    for(int i = 1; i < n; i++){
+        for(int N = 1; N <= n; N++){
+            int notPick = dp[i-1][N];
+            int pick = -1e9;
+            int rodLen = i+1;
+            if (rodLen <= N)
+                pick = price[i] + dp[i][N-rodLen];
+            dp[i][N] = max(pick, notPick);
+        }
+    }
+    return dp[n-1][n];
+}
+
+
 //MEMOIZATION
 int f(vector<int>& price, int i, int N, vector<vector<int>>& dp){
     if (i == 0){
