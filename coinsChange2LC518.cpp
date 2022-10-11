60446e12c02936c50cd7695048a61b2551c48bb5
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-10-12 00:55:55 +0530

    Update coinsChange2LC518.cpp

--- coinsChange2LC518.cpp
+++ coinsChange2LC518.cpp
@@ -1,3 +1,24 @@
+//TABULATION
+int change(int amount, vector<int>& coins) {
+  int n = coins.size();
+  vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+  for(int tar = 0; tar <= amount; tar++){
+      if(tar % coins[0] == 0)
+          dp[0][tar] = 1;
+      else
+          dp[0][tar] = 0;
+  }
+  for(int i = 1; i < n; i++){
+      for(int tar = 0; tar <= amount; tar++){
+          int notPick = dp[i-1][tar], pick = 0;
+          if (tar - coins[i] >= 0)  
+              pick = dp[i][tar-coins[i]];
+          dp[i][tar] = pick + notPick;
+      }
+  }
+  return dp[n-1][amount];
+}
+
 //MEMOIZATION
 int f(int tar, vector<int>& arr, int i, vector<vector<int>>& dp){
       if (i == 0){
