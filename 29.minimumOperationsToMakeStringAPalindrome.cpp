93ba4ca8930fda208998a7cb818ae274344ffdbc
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2022-10-26 20:00:55 +0530

    Space Optimization

---
+++ 29.minimumOperationsToMakeStringAPalindrome.cpp
@@ -0,0 +1,23 @@
+//SPACE OPTIMIZATION
+int minInsertions(string s) {
+    string text1 = s;
+    reverse(s.begin(), s.end());
+    string text2 = s;
+    vector<int> prev(text2.size()+1, 0);
+    for(int i2 = 0; i2 <= text2.size(); i2++){
+        prev[i2] = 0;
+    }
+    for(int i1 = 1; i1 <= text1.size(); i1++){
+        vector<int> curr(text2.size()+1, 0);
+        curr[0] = 0;
+        for(int i2 = 1; i2 <= text2.size(); i2++){
+            if (text1[i1-1] == text2[i2-1])
+                curr[i2] = 1 + prev[i2-1];
+            else
+                curr[i2] = max(prev[i2], curr[i2-1]);
+        }
+        prev = curr;
+    }
+    int lps = prev[text2.size()];
+    return s.size() - lps;
+}
